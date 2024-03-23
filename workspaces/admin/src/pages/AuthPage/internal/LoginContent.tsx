561127e6d79b5a08f4db5c79f2c0a7a3442f5fe9
raorao1011 <raorao1011@gmail.com>
2024-03-23 21:32:08 +0900

    世紀表現直す

--- workspaces/admin/src/pages/AuthPage/internal/LoginContent.tsx
+++ workspaces/admin/src/pages/AuthPage/internal/LoginContent.tsx
@@ -24,14 +24,14 @@ export const LoginContent: React.FC = () => {
         .required('メールアドレスを入力してください')
         .test({
           message: 'メールアドレスには @ を含めてください',
-          test: (v) => /^(?:[^@]*){12,}$/v.test(v) === false,
+          test: (v) => new RegExp(/^[^@]+@[^@]+\.[^@]+$/).test(v),
         }),
       password: yup
         .string()
         .required('パスワードを入力してください')
         .test({
           message: 'パスワードには記号を含めてください',
-          test: (v) => /^(?:[^\P{Letter}&&\P{Number}]*){24,}$/v.test(v) === false,
+          test: (v) => new RegExp(/.*[\W_].*/).test(v),
         }),
     }),
   });
