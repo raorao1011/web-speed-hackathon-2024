565d32a349fa3ff4d45ebf830bdffc5ebe853a00
raorao1011 <raorao1011@gmail.com>
2024-03-23 20:16:29 +0900

    Revert "fix updateViaCache"
    This reverts commit 9774c3bd3e517d63e9f6e4fd17b0d8e14aaca51b.

--- workspaces/client/src/utils/registerServiceWorker.ts
+++ workspaces/client/src/utils/registerServiceWorker.ts
@@ -1,6 +1,6 @@
 export async function registerServiceWorker() {
   const registration = await navigator.serviceWorker
-    .register('/serviceworker.global.js', { updateViaCache: "imports" })
+    .register('/serviceworker.global.js', { updateViaCache: 'none' })
     .then(() => navigator.serviceWorker.ready);
 
   // Wait until the service worker becomes active
