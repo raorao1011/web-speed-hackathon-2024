8db87b0cdb642e7c454cd1efb1b0604cf9e821b2
raorao1011 <raorao1011@gmail.com>
2024-03-24 14:12:53 +0900

    footerのlayout shift直す

--- workspaces/app/src/foundation/components/Footer.tsx
+++ workspaces/app/src/foundation/components/Footer.tsx
@@ -110,7 +110,7 @@ export const Footer: React.FC = () => {
   };
 
   return (
-    <Box as="footer" backgroundColor={Color.Background} p={Space * 1}>
+    <Box as="footer" backgroundColor={Color.Background} maxHeight="90px" p={Space * 1}>
       <Flex align="flex-start" direction="column" gap={Space * 1} justify="flex-start">
         <img alt="Cyber TOON" src="/assets/cyber-toon.webp" />
         <Flex align="start" direction="row" gap={Space * 1.5} justify="center">
